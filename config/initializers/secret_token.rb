8e3f3c2f54fcb36a3d54150c194d8edcc4be46d2
Fernando Saenz <fsaenz@despegar.com>
2012-10-30 22:00:59 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssReader::Application.config.secret_token = '8a6dd414bf1fef68bf499915f4e1a82a7e2ea095bb10666bf524f8bf6c5ce9d862264ce03a652f397becc21fdfedc4e1c36e62aed0ef0f38efbbca9c36f88cfe'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+RssReader::Application.routes.draw do
+  devise_for :users
+
+  root :to => "home#index"
+end
+
