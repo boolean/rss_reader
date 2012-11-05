bb28c7a54e35f8d6fa4b50e310d192c7be4143ae
Fernando Saenz <fsaenz@despegar.com>
2012-11-05 01:34:08 -0300

    added confirmable attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121105035246) do
+ActiveRecord::Schema.define(:version => 20121105042116) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -99,6 +99,10 @@ ActiveRecord::Schema.define(:version => 20121105035246) do
     t.datetime "created_at",                                           :null => false
     t.datetime "updated_at",                                           :null => false
     t.string   "avatar"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
