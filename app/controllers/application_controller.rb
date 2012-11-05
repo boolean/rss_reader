6a0faac2e66a65742389c1a2e45bcd740f3c54dd
Fernando Saenz <fsaenz@despegar.com>
2012-11-04 23:52:29 -0300

    redirect to index with flash message in case of error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,21 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   before_filter :load_channels
 
+  rescue_from ActionController::RoutingError, :with => :rescue_not_found
+  rescue_from ActionController::UnknownAction, :with => :rescue_not_found
+  rescue_from ActiveRecord::RecordNotFound, :with => :rescue_not_found
+
   protect_from_forgery
+
+  protected
+
+  def rescue_not_found
+    redirect_to root_path, :error => 'Page Not Found'
+  end
 
   private
 
   def load_channels
-    if current_user
-      @channels = current_user.channels
-    end
+    @channels = current_user.channels if current_user
   end
 end
